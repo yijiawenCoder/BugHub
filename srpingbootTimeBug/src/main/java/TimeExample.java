1ab8ae151926775c054271e7e7d7cd1c65e69c14
yijiawenCoder <131642520+yijiawenCoder@users.noreply.github.com>
2023-12-27 17:13:45 +0800

    Add files via upload
    数据库时间以及驼峰命名法的问题

---
+++ srpingbootTimeBug/src/main/java/TimeExample.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class TimeExample {
+
+    @Test
+    public void testTime() {
+       /**
+        *  测试时间相关功能
+        *         user.setCreateTime(new Timestamp(new java.util.Date().getTime()));
+        *         注意Timestamp需要导入java.sql包,表示时间戳stamp的s小写
+        * **/
+
+    }
+}
+
+
+
